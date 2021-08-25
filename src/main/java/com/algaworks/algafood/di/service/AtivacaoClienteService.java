ec5f8e92a7253be9f7a41a68c7b6f1bdd8169cd5
Carlos Silva <carlos.alexsilva@gmail.com>
2021-08-24 22:25:11 -0300

    Padrão de projeto OBSERVER dentro do Spring é conhecido como EventHandler. Chamada a notificação e emissão de nota através de evento, disparado ao ativar cliente, usando o conceito do OBSERVER.

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -6,6 +6,7 @@ import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
@@ -16,42 +17,13 @@ import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 @Component
 public class AtivacaoClienteService {
 
-	//Podemos indicar o @Autowired aqui também para indicar que esse atributo será instanciado juntamente com essa classe.
-	//Mas ai não é possivel indicar outro tipo de notificador, se no caso tivermos mais que um tipo de notificação.
-	
-	@TipoDoNotificador(NivelUrgencia.URGENTE)
 	@Autowired
-	private Notificador notificador;
+	private ApplicationEventPublisher eventPublisher;
 	
-	
-	//É possível indicar o método que será utilizado nos ciclos de vida do bean sem as anotações @PostConstruct e PreDestroy
-	//Usando: @Bean(initMethod = "init", destroyMethod="destroy"), isso poderia ser usado em uma classe de configuração como em aula anterior
-	
-	@PostConstruct
-	public void init() {
-		System.out.println("INIT");
-	}
-	
-	@PreDestroy
-	public void destroy() {
-		System.out.println("DESTROY");
-	}
-
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		
-		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");	
-		
+		//dizer para o container que o client esta ativo neste momento, através do Evento	
+		eventPublisher.publishEvent(new ClienteAtivadoEvent(cliente));
 	}
-		
-	/*
-	 * podemos indicar também um método para ser o default na instanciação.
-	@Autowired
-	public void setNotificador(Notificador notificador) {
-		this.notificador = notificador;
-	}
-	*/
-	
-	
-	
-}
+		}
