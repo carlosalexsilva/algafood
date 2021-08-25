adcc1b3da341576f90985975a6634b853d943707
Carlos Silva <carlos.alexsilva@gmail.com>
2021-08-24 23:55:09 -0300

    Configurando arquivos de propriedades por ambiente, onde o principal application.properties recebe o atributo que indica o arquivo de dev ou prod e usa as configurações dele, isso pode ser alterado por linha de cmd.
    Não usar dados sensiveis tipo senhas de banco ou servidor em arquivos .properties para produção, o recomendado é alterar a propriedade pela linha de comando ou variavel de ambiente.

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +11,13 @@ import com.algaworks.algafood.di.modelo.Cliente;
 @Component
 public class NotificadorEmailMock implements Notificador  {
 
+	@Autowired
+	private NotificadorProperties properties;
 		
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		System.out.println("Host: " + properties.getHostServidor());
+		System.out.println("Porta: " + properties.getPortaServidor());
 		System.out.printf("Notificação seria por email para: %s através do e-mail %s: %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
