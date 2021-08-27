8a5165ddb54bb926c28cb526c80ee29e6ee17740
Carlos Silva <carlos.alexsilva@gmail.com>
2021-08-27 01:04:49 -0300

    Utilizando lombok. A notação @Data faz todos gera todos os 3 itens Getter, Setter e EqualsAndHashCode, porém o equals é feito para todos os atributos, se quiser especificar somente um arquivo é necessário usar conforme no exemplo.

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -5,57 +5,24 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+//@Getter
+//@Setter
+//@EqualsAndHashCode
+@Data //O Data faz todos os 3 acima de uma vez Getter, Setter e EqualsAndHashCode
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) //indica que EqualsAndHashCode só será feito para o campo explicitamente identificado, 
+												  //pois o Data gera para todos os atributos
 @Entity
 //@Table(name = "cozinha") - comentado pq a tabela vai chamar cozinha
 public class Cozinha {
 
+	@EqualsAndHashCode.Include
 	@Id //chave primary da Tabela
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	private String nome;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cozinha other = (Cozinha) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
-	
+	private String nome;	
 	
 }
