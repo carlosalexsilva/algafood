64524af42fabf3e07ebdadc720634b5b273172b4
Carlos Silva <carlos.alexsilva@gmail.com>
2021-08-27 00:49:53 -0300

    Repositorio é uma camada que possui interface de uma Classe que abstrai qual é o implementador da persistencia. infrastructure é tudo que não tem haver com o negócio, tipo enviar um email, não tem haver com a regra do negocio, mas é algo que precisa ser feito pela infra.
    O PADRÃO PARA CRIAÇÃO DE REPOSITÓRIO É CRIAR UM REPOSITORIO POR AGGREGATEROOT E NÃO UM REPOSITORY POR ENTITY.
    Exemplo tenho Pedido e ItemPedido, vou criar um PedidoRepository que irá tratar as duas entities.

--- src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -1,41 +1,40 @@
-package com.algaworks.algafood.jpa;
+package com.algaworks.algafood.infrastructure.repository;
 
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
-@Component
-public class CadastroCozinha {
+public class CozinhaRepositoryImpl implements CozinhaRepository{
 
 	@PersistenceContext // Injetando o EntityManager
 	private EntityManager manager;
 	
+	@Override
 	public List<Cozinha> listar() {
-		//somente o from Cozinha, traz todos os atributes da cozinha
-		//TypedQuery<Cozinha> query = manager.createQuery("from Cozinha", Cozinha.class);		
-		//return query.getResultList();
 		
 		return manager.createQuery("from Cozinha", Cozinha.class)
 				.getResultList();
 	}
 	
+	@Override
 	public Cozinha buscar(Long id) {
 		return manager.find(Cozinha.class, id);
 	}
 	
+	@Override
 	@Transactional
 	public Cozinha salvar(Cozinha cozinha) {
 		return manager.merge(cozinha);
 		
 	}
 	
+	@Override
 	@Transactional
 	public void remover(Cozinha cozinha) {
 		
@@ -43,4 +42,6 @@ public class CadastroCozinha {
 		
 		manager.remove(cozinha);		
 	}
+
+	
 }
