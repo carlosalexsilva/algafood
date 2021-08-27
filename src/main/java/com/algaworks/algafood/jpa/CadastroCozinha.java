4c0248bf0cc2cc6007669b3a57f4ef38631f10ad
Carlos Silva <carlos.alexsilva@gmail.com>
2021-08-27 00:00:04 -0300

    Método merge insere o registro e retorna o objeto já com o id criado. @Transactional - executa o método dentro de uma trasação.

--- src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -7,6 +7,7 @@ import javax.persistence.PersistenceContext;
 
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 
@@ -23,5 +24,11 @@ public class CadastroCozinha {
 		
 		return manager.createQuery("from Cozinha", Cozinha.class)
 				.getResultList();
+	}
+	
+	@Transactional
+	public Cozinha adicionar(Cozinha cozinha) {
+		return manager.merge(cozinha);
+		
 	}
 }
---
+++ src/main/java/com/algaworks/algafood/jpa/InclusaoCozinhaMain.java
@@ -0,0 +1,34 @@
+package com.algaworks.algafood.jpa;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.domain.model.Cozinha;
+
+public class InclusaoCozinhaMain {
+
+	public static void main(String[] args) {
+	
+		//COMANDO UTILIZADO PARA INSTANCIAR A APLICAÇÃO MAS NÃO EM MODO WEB, OU SEJA INICIA E JÁ MORRE A APLICAÇÃO
+		//NÃO FICA AGUARDANDO UMA REQUISIÇÃO COMO NO MODO WEB.
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Brasileira");
+		
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Japonesa");
+		
+		cozinha1 = cadastroCozinha.adicionar(cozinha1);
+		cozinha2 = cadastroCozinha.adicionar(cozinha2);
+		
+		System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
+		System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
+	}
+}
