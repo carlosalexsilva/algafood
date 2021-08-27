fdc5e2e2cbd8bcf84f2e4fa8c6b6d1e343e382b7
Carlos Silva <carlos.alexsilva@gmail.com>
2021-08-26 23:08:50 -0300

    Validando como gerar AUTO-INCREMENT para o Id baseado no mysql

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,7 +1,8 @@
 package com.algaworks.algafood.domain.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -9,9 +10,9 @@ import javax.persistence.Id;
 public class Cozinha {
 
 	@Id //chave primary da Tabela
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "nom_cozinha")
 	private String nome;
 
 	public Long getId() {
