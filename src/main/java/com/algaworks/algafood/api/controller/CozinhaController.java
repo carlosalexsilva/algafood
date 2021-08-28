0107f156bc238c9d20d509ab82ecf01089d59d01
Carlos Silva <carlos.alexsilva@gmail.com>
2021-08-28 00:45:23 -0300

    PathVariable é a notação que vincula a variavel definida no GetMapping do método com o nome da variavel do método.

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,8 +3,8 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,17 +12,21 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
-//@RequestMapping("/cozinhas")
-@RequestMapping(value = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE) // aqui a classe inteira irá responder somente em JSON
+@RequestMapping("/cozinhas")
 public class CozinhaController {
 	
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-	
-	//@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE) - isso indica que esse método só poderá retornar resposta json
-	//@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }) //usando array para permitir mais de um tipo
+
+	@GetMapping
 	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
+	}
+	
+	@GetMapping("/{cozinhaId}") // => {cozinhaId} é conhecido como PathVariable
+	//public Cozinha buscar(@PathVariable("cozinhaId") Long id) { => Poderia ser feito assim também
+	public Cozinha buscar(@PathVariable Long cozinhaId) {
+		return cozinhaRepository.buscar(cozinhaId);
 	}
 	
 }
