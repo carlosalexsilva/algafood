94c504a9ff4eeb0fac87d83e68b67b1b3de59610
Carlos Silva <carlos.alexsilva@gmail.com>
2021-08-28 00:32:42 -0300

    Utilizando atributo "produces" para forçar o tipo de resposta que a API irá responder, existem os escopo da resposta na classe e nos métodos conforme comentários nas classe, também podemos responder o mesmo método chamado com verbo GET diferenciando o listar_1 e listar_2 pelo tipo de reposta ou seja listar_1 para json e lsitar_2 para xml.

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,15 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping("/cozinhas")
+//@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE) // aqui a classe inteira irá responder somente em JSON
 public class CozinhaController {
 	
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-	@GetMapping
+	//@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE) - isso indica que esse método só poderá retornar resposta json
+	//@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }) //usando array para permitir mais de um tipo
 	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
 	}
