ee68bf5edf52f645b2bb99d28a0367c7065bd6c9
Carlos Silva <carlos.alexsilva@gmail.com>
2021-08-28 00:56:24 -0300

    Propriedades do jackson que permite transformar a resposta da API, assim como o transformer do Laravel fazia. @JsonRootName("gastronomia") -> usado para xml altera o nome do nível para essa classe @JsonIgnore -> nao exibe esse atributo na resposta . @JsonProperty("titulo") -> altera o nome do atributo somente no retorno do servidor.

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -6,12 +6,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-//@Getter
-//@Setter
-//@EqualsAndHashCode
+@JsonRootName("gastronomia") // -> usado para xml altera o nome do nível para essa classe
 @Data //O Data faz todos os 3 acima de uma vez Getter, Setter e EqualsAndHashCode
 @EqualsAndHashCode(onlyExplicitlyIncluded = true) //indica que EqualsAndHashCode só será feito para o campo explicitamente identificado, 
 												  //pois o Data gera para todos os atributos
@@ -24,6 +26,8 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	//@JsonIgnore -> nao exibe esse atributo na resposta 
+	@JsonProperty("titulo") // -> altera o nome do atributo somente no retorno do servidor, como se fosse o transformer do laravel
 	@Column(nullable = false)
 	private String nome;	
 	
