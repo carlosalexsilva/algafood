baec771ca74fe6157c1336970d9e32690cfcc400
Carlos Silva <carlos.alexsilva@gmail.com>
2021-08-29 23:44:48 -0300

    Essas são as alterações referente ao branch 04.20.

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,15 +3,14 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
@@ -27,16 +26,27 @@ public class CozinhaController {
 		return cozinhaRepository.listar();
 	}
 	
-	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public CozinhasXmlWrapper listarXml() {
-		return new CozinhasXmlWrapper(cozinhaRepository.listar());
-	}
-	
 	@GetMapping("/{cozinhaId}") // => {cozinhaId} é conhecido como PathVariable
 	//public Cozinha buscar(@PathVariable("cozinhaId") Long id) { => Poderia ser feito assim também
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
 		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
-		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+		
+		//return ResponseEntity.status(HttpStatus.OK).body(cozinha); modelo para definir o status que eu quiser usando o ResponseEntity
+		
+		//return ResponseEntity.ok(cozinha); 
+		// essa linhha é um atalho para o tipo de resposta da linha acima, porém a linha acima é possível 
+		//criar ifs para condicionar a resposta
+		
+		// No exemplo abaixo, estamos usando o responde de um found que indica que a página foi redirecionada temporariamente
+		// para outra url, por isso um teste simples passando o listar todas cozinhas.
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.LOCATION, "http://localhost/cozinhas");
+		
+		return ResponseEntity
+				.status(HttpStatus.FOUND)
+				.headers(headers)
+				//.body(cozinha);
+				.build();
 	}
 	
 }
