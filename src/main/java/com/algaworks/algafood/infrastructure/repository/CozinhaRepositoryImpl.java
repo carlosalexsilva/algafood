e3a52e498e01d2ac1006036aecacd4d7df429185
Carlos Silva <carlos.alexsilva@gmail.com>
2021-08-31 23:11:43 -0300

    Refatorando método excluir para usar camada de serviços.

--- src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,10 +39,12 @@ public class CozinhaRepositoryImpl implements CozinhaRepository{
 	
 	@Override
 	@Transactional
-	public void remover(Cozinha cozinha) {
+	public void remover(Long id) {
 		
-		cozinha = buscar(cozinha.getId());
-		
+		Cozinha cozinha = buscar(id);
+		if (cozinha == null) {
+			throw new EmptyResultDataAccessException(1); //para é a quantidade de cozinhas que esperava
+		}
 		manager.remove(cozinha);		
 	}
 
--- src/main/java/com/algaworks/algafood/jpa/ExclusaoCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/ExclusaoCozinhaMain.java
@@ -23,7 +23,7 @@ public class ExclusaoCozinhaMain {
 		Cozinha cozinha = new Cozinha();
 		cozinha.setId(1L);
 		
-		cozinhaRepository.remover(cozinha);
+		cozinhaRepository.remover(1L);
 		
 	}
 }
