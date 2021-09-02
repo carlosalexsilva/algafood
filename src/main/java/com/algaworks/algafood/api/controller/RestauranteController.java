f7209bb093a69b51fdfe3226c7766ee7a52dad50
Carlos Silva <carlos.alexsilva@gmail.com>
2021-09-01 23:31:58 -0300

    Iniciando atualização utilizando o PATCH, com o principio que todos os campos repassados na requisição devem ser atualizados e os que não foram passados continuam iguais. Se isso ocorresse usando o PUT os campos não informados seriam atualizados com NULL, por isso foi utilizado o MAP para recuperar somente os campos informados.

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -95,5 +97,17 @@ public class RestauranteController {
 		} catch (EntidadeEmUsoException e) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
+	}
+	
+	@PatchMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId, 
+			@RequestBody Map<String, Object> campos) {
+		
+		campos.forEach((nomePropriedade, valorPropriedade) -> {
+			System.out.println(nomePropriedade + " - " + valorPropriedade);
+		}
+		);
+				
+		return ResponseEntity.ok().build();
 	}
 }
