973b160f4d9ed5321d9ab1b766aa54300c6a5788
Carlos Silva <carlos.alexsilva@gmail.com>
2021-09-05 15:58:37 -0300

    Gerando consultas completas. Usar sempre o sufixo Impl ao final do nome da classe exemplo RestauranteRepositoryImpl. Através da classe RestauranteRepositoryImpl, fazemos uma extração de interface "refactor => extract interface" para que essa classe utilize a nova interface e essa nova é implementada no RestauranteRepository, isso nos garante a integridade do nome da classe, impedindo que uma alteração no nome do método na classe Impl não de erro.

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -70,4 +70,11 @@ public class TesteController {
 		return restauranteRepository.countByCozinhaId(cozinhaId);
 	}
 	
+	@GetMapping("/restaurantes/por-nome-e-frete")
+	public List<Restaurante> restaurantePorNomeFrete(
+			String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+
+		return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
+	}
+	
 }
