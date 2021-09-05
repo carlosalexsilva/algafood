4d62b189f555523dff6957cbd53a6208c9116685
Carlos Silva <carlos.alexsilva@gmail.com>
2021-09-05 16:33:55 -0300

    Criando predicados para gerar o where da consulta.

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -8,6 +8,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
@@ -28,7 +30,14 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 		
 		//CriteriaQuery - é um construtor de clausulas		
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-		criteria.from(Restaurante.class); // from Restaurante
+		Root<Restaurante> root = criteria.from(Restaurante.class); // from Restaurante (root) é o Restaurante
+		
+		//Predicate é como um filtro, passar sempre o atributo através do root.get
+		Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
+		Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+		Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+		
+		criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
 		
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);		
 		return query.getResultList();
