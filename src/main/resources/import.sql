97ccb0e198e20ca0660a3d4bfbdb6e7a58bced6a
Carlos Silva <carlos.alexsilva@gmail.com>
2021-09-06 07:21:15 -0300

    Sempre analisar se o consumidor realmente precisa de todos os dados, no listar as vezes é bom só enviar o necessário, pois o payload pode ficar muito carregado. Nesse exemplo estamos ignorando a serialização do endereço, bem como ignorando o endereço para atualizar.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,16 @@
 insert into cozinha (nome)values('Chinesa');
 insert into cozinha (nome)values('Japonesa');
-insert into restaurante (nome,taxa_frete, cozinha_id)values('Saiko',5, 2);
+
+insert into estado (nome)values('Paraná');
+insert into estado (nome)values('São Paulo');
+
+insert into cidade (nome, estado_id)values('Londrina', 1);
+insert into cidade (nome, estado_id)values('Cambé', 1);
+insert into cidade (nome, estado_id)values('Rolândia', 1);
+insert into cidade (nome, estado_id)values('São Paulo', 2);
+insert into cidade (nome, estado_id)values('Guarulhos', 2);
+
+insert into restaurante (nome,taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro)values('Saiko',5, 2, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
 insert into restaurante (nome,taxa_frete, cozinha_id)values('JinJin', 7, 1);
 insert into restaurante (nome,taxa_frete, cozinha_id)values('China In Box', 10, 1);
 
@@ -13,13 +23,5 @@ insert into permissao (nome, descricao)values('ConsultarPedido', 'Consulta Pedid
 insert into permissao (nome, descricao)values('ConfirmarPedido', 'Aceita pedido realizado pelo cliente');
 insert into permissao (nome, descricao)values('EntregaPedido', 'Confirma a retirada do pedido no balcão e finaliza Entrega.');
 
-insert into estado (nome)values('Paraná');
-insert into estado (nome)values('São Paulo');
-
-insert into cidade (nome, estado_id)values('Londrina', 1);
-insert into cidade (nome, estado_id)values('Cambé', 1);
-insert into cidade (nome, estado_id)values('Rolândia', 1);
-insert into cidade (nome, estado_id)values('São Paulo', 2);
-insert into cidade (nome, estado_id)values('Guarulhos', 2);
 
 insert into restaurante_forma_pagamento(restaurante_id, forma_pagamento_id)values (1,1), (1,2), (1,3), (1,4), (2,1), (2,2), (3, 1);
